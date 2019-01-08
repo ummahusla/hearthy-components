d0133c5e23a05e91d76ec874099b81b496489365
Edvins Antonovs <ummahusla@gmail.com>
2019-01-08 09:32:47 +0000

    Created few generic components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Button from "./components/Atoms/Button/index.jsx";
+import Button from './components/Atoms/Button/index.jsx';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+
       <h2>Start editing to see some magic happen!</h2>
       <Button />
     </div>
   );
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
